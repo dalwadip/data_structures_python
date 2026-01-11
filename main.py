2c19b75fa1156495c973db5f340629004fe98cb2
P Dalwadi <dalwad63@rowan.edu>
2026-01-11 18:46:53 -0500

    Adding a file

---
+++ main.py
@@ -0,0 +1,16 @@
+# make list of items
+person = ["Harry", "Potter", 16, True]
+
+# make a set
+fruits = {"banana", "apple", "cherry"}
+
+# make a dictionary
+student = {
+    "name": "Andy",
+    "age": 22,
+    "is_kind": False
+}
+
+# make a tuple
+sports = ("soccer", "basketball", "tennis")
+
